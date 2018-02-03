34535d76698d806b3cc7b79d69a58ecfbe53524a
Marek Horban <maho@netcompany.com>
2018-02-04 00:26:56 +0100

    Refactoring

--- src/main/java/pl/horban/camel/twitterexample/TwitterSearchProcessor.java
+++ src/main/java/pl/horban/camel/twitterexample/TwitterSearchProcessor.java
@@ -6,6 +6,8 @@ import org.apache.camel.Processor;
 
 public class TwitterSearchProcessor implements Processor {
 
+    private String NEW_LINE = System.lineSeparator();
+
     static final String FOLLOWERS_COUNT_HEADER = "followersCountHeader";
     static final String LANG_HEADER = "langHeader";
 
@@ -13,16 +15,16 @@ public class TwitterSearchProcessor implements Processor {
     public void process(Exchange exchange) throws Exception {
         twitter4j.Status twitterStatus = exchange.getIn().getBody(twitter4j.Status.class);
 
-        TwitterObject tweet = new TwitterObject();
-        tweet.setDate(twitterStatus.getCreatedAt());
-        tweet.setUserName(twitterStatus.getUser().getName());
-        tweet.setLang(twitterStatus.getUser().getLang());
-        tweet.setFollowersCount(twitterStatus.getUser().getFollowersCount());
-        tweet.setTweet(twitterStatus.getText());
+        String tweet = "TIMESTAMP: " + twitterStatus.getCreatedAt() + NEW_LINE +
+                "USER_NAME: " + twitterStatus.getUser().getName() + NEW_LINE +
+                "LANG: " + twitterStatus.getUser().getLang() + NEW_LINE +
+                "FOLLOWERS_COUNT: " + twitterStatus.getUser().getFollowersCount() + NEW_LINE +
+                "TWEET: " + twitterStatus.getText() + NEW_LINE;
 
-        exchange.getIn().setBody(tweet.toString());
         exchange.getIn().setHeader(Exchange.FILE_NAME, twitterStatus.getId() + ".txt");
-        exchange.getIn().setHeader(FOLLOWERS_COUNT_HEADER, tweet.getFollowersCount());
-        exchange.getIn().setHeader(LANG_HEADER, tweet.getLang());
+        exchange.getIn().setHeader(FOLLOWERS_COUNT_HEADER, twitterStatus.getUser().getFollowersCount());
+        exchange.getIn().setHeader(LANG_HEADER, twitterStatus.getUser().getLang());
+        
+        exchange.getIn().setBody(tweet);
     }
 }
